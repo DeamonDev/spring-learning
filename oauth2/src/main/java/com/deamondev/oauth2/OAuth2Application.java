d4750fbf8d2dddf75cabd270fbd975103648152b
Piotr Rudnicki <deamondev@gmail.com>
2022-09-12 11:43:00 +0200

    logout

--- oauth2/src/main/java/com/deamondev/oauth2/OAuth2Application.java
+++ oauth2/src/main/java/com/deamondev/oauth2/OAuth2Application.java
@@ -11,12 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Oauth2Application extends WebSecurityConfigurerAdapter {
+public class OAuth2Application extends WebSecurityConfigurerAdapter {
 
   @GetMapping("/user")
   public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
@@ -28,11 +29,13 @@ public class Oauth2Application extends WebSecurityConfigurerAdapter {
     http.authorizeRequests(a -> a.antMatchers("/", "/error", "/webjars/**").permitAll()
         .anyRequest().authenticated())
         .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .logout(l -> l.logoutSuccessUrl("/").permitAll())
         .oauth2Login();
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(Oauth2Application.class, args);
+    SpringApplication.run(OAuth2Application.class, args);
   }
 
 }
